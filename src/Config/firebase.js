ac3a9bd426dc19f5a4db53664b2f98031316591f
phildehovre <66724307+phildehovre@users.noreply.github.com>
2022-10-31 08:56:55 +0000

    dot env

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD8G3SJ3rVlbCgPkePygyW08YahiOdnkDA",
-    authDomain: "myfi-cbf74.firebaseapp.com",
-    projectId: "myfi-cbf74",
-    storageBucket: "myfi-cbf74.appspot.com",
-    messagingSenderId: "1006343653715",
-    appId: "1:1006343653715:web:7c770d49329e0a34f5f9a6",
-    measurementId: "G-HKYX11SSCL"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURMENT_ID
 };
 
 
